d9a9e8edf5d17989de78d5ffab8de48c4a209069
shaivya05 <130532388+shaivya05@users.noreply.github.com>
2024-06-17 15:27:15 +0530

    intergrated api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function refresh(response) {
+  console.log(response.data.temperature.current);
+
+  let t = document.querySelector("#value-temp");
+  let temp = response.data.temperature.current;
+  t.innerHTML = Math.round(temp);
+  let c = document.querySelector("#city");
+  c.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let api = "5001994a3a9f98fb4t8c2o6a0bc2953d";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}`;
+  axios.get(apiURL).then(refresh);
+}
+
 function dooo(event) {
   event.preventDefault();
   let b = document.querySelector("#search-form-input");
   let c = document.querySelector("#city");
-  c.innerHTML = b.value;
+  //   c.innerHTML = b.value;
+  searchCity(b.value);
 }
 
 let a = document.querySelector("#search");
